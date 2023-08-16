53f0614240adf14dbaf590cd78306db21e59fb4b
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-08-16 15:20:04 +0200

    KursAngular - ProjektWspólny - Password reset

--- KursAngular/projekt-wspolny/src/app/modules/auth/components/password-recovery/password-recovery.component.ts
+++ KursAngular/projekt-wspolny/src/app/modules/auth/components/password-recovery/password-recovery.component.ts
@@ -1,5 +1,8 @@
+import { AuthService } from './../../../core/services/auth.service';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
 import { RecoveryForm } from 'src/app/modules/core/models/forms.model';
 import { FormService } from 'src/app/modules/core/services/form.service';
 
@@ -11,7 +14,16 @@ import { FormService } from 'src/app/modules/core/services/form.service';
 export class PasswordRecoveryComponent {
   recoveryForm: FormGroup<RecoveryForm> = this.formService.initRecoveryForm();
 
-  constructor(private formService: FormService) {}
+  constructor(
+    private formService: FormService,
+    private authService: AuthService,
+    private notifierService: NotifierService,
+    private router: Router,
+  ) {}
+
+  errorMsg: string | null = null;
+
+  loading = false;
 
   get controls() {
     return this.recoveryForm.controls;
@@ -19,5 +31,22 @@ export class PasswordRecoveryComponent {
 
   getErrorMessage(control: FormControl): string {
     return this.formService.getErrorMessage('email', control);
+  }
+
+  onPasswdRecovery() {
+    this.loading = true;
+    this.authService.resetPassword(this.recoveryForm.getRawValue()).subscribe({
+      next: () => {
+        this.notifierService.notify(
+          'success',
+          'Wiadomość z linkiem do odzyskiwania hasła została wysłana na wskazany adres e-mail.',
+        );
+        this.router.navigate(['/logowanie']);
+      },
+      error: (err) => {
+        this.loading = false;
+        this.errorMsg = err;
+      },
+    });
   }
 }
--- KursAngular/projekt-wspolny/src/app/modules/auth/components/reset-info/reset-info.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-reset-info',
-  templateUrl: './reset-info.component.html',
-  styleUrls: ['./reset-info.component.scss'],
-})
-export class ResetInfoComponent {}
