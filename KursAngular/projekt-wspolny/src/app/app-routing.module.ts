c324fe0dff8e13093935e7d5d62c740d3b1e3ada
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-09-07 13:32:25 +0200

    KursAngular - ProjektWspólny - Inicjalizacja modułu produducts, home i utworzenie linku z opcja dropdown

--- KursAngular/projekt-wspolny/src/app/app-routing.module.ts
+++ KursAngular/projekt-wspolny/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'produkty',
+    loadChildren: () =>
+      import('./modules/products/products.module').then(
+        (m) => m.ProductsModule,
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- KursAngular/projekt-wspolny/src/app/modules/core/components/header/header.component.ts
+++ KursAngular/projekt-wspolny/src/app/modules/core/components/header/header.component.ts
@@ -5,6 +5,7 @@ import * as AuthActions from '../../../auth/store/auth.actions';
 import { Observable } from 'rxjs';
 import { User } from '../../models/auth.model';
 import { selectAuthUser } from 'src/app/modules/auth/store/auth.selectors';
+import { Category } from '../../models/categories.model';
 
 @Component({
   selector: 'app-header',
@@ -15,6 +16,13 @@ export class HeaderComponent {
   constructor(private store: Store<AppState>) {}
 
   user$: Observable<User | null> = this.store.select(selectAuthUser);
+
+  categories: Category[] = [
+    { name: 'Kategoria 1', shortId: 123456 },
+    { name: 'Kategoria 2', shortId: 123457 },
+    { name: 'Kategoria 3', shortId: 123458 },
+    { name: 'Kategoria 4', shortId: 123459 },
+  ];
 
   logout() {
     this.store.dispatch(AuthActions.logout());
