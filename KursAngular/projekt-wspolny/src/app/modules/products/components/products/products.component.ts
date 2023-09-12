4e1a295835203536ce4a44e120c83e1edc1bdd84
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-09-12 16:29:20 +0200

    KursAngular - ProjektWspólny - Pobieranie produktów z stronnicowaniem (_limit i _page) 2

--- KursAngular/projekt-wspolny/src/app/modules/products/components/products/products.component.ts
+++ KursAngular/projekt-wspolny/src/app/modules/products/components/products/products.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { map, startWith, switchMap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription, map, startWith, switchMap } from 'rxjs';
 import { PrimitiveProduct } from 'src/app/modules/core/models/product.model';
 import { ProductsService } from 'src/app/modules/core/services/products.service';
 
@@ -9,8 +10,16 @@ import { ProductsService } from 'src/app/modules/core/services/products.service'
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent implements AfterViewInit {
-  constructor(private productService: ProductsService) {}
+export class ProductsComponent implements AfterViewInit, OnDestroy {
+  constructor(
+    private productService: ProductsService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   ngAfterViewInit(): void {
     // this.productService.getProducts().subscribe({
@@ -20,24 +29,48 @@ export class ProductsComponent implements AfterViewInit {
     //   },
     // });
 
-    this.paginator.page
+    this.route.queryParamMap
       .pipe(
-        startWith({}),
-        switchMap(() => {
-          const pageIndex = this.paginator.pageIndex + 1;
-          const itemsPerPage = this.paginator.pageSize;
-          return this.productService.getProducts(pageIndex, itemsPerPage);
+        switchMap((queryMap) => {
+          const pageIndex = queryMap.get('strona')
+            ? Number(queryMap.get('strona'))
+            : 1;
+          const limit = queryMap.get('limit')
+            ? Number(queryMap.get('limit'))
+            : this.paginator.pageSize;
+          return this.productService.getProducts(pageIndex, limit);
         }),
         map((response) => {
           this.products = [...response.products];
           this.totalCount = response.totalCount;
         }),
       )
-      .subscribe();
+      .subscribe({
+        error: (err) => {
+          this.error = err;
+        },
+      });
+
+    this.sub.add(
+      this.paginator.page.subscribe({
+        next: () => {
+          const pageIndex = this.paginator.pageIndex + 1;
+          const itemsPerPage = this.paginator.pageSize;
+          this.router.navigate([], {
+            relativeTo: this.route,
+            queryParams: { strona: pageIndex, limit: itemsPerPage },
+          });
+        },
+      }),
+    );
   }
 
   products: PrimitiveProduct[] = [];
   totalCount = 0;
+
+  sub = new Subscription();
+
+  error: string | null = null;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 }
--- KursAngular/projekt-wspolny/src/app/modules/shared/shared.module.ts
+++ KursAngular/projekt-wspolny/src/app/modules/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/material.module/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AlertComponent],
   imports: [CommonModule],
-  exports: [CommonModule, MaterialModule, ReactiveFormsModule],
+  exports: [CommonModule, MaterialModule, ReactiveFormsModule, AlertComponent],
 })
 export class SharedModule {}
